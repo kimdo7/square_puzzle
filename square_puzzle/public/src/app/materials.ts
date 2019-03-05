f665a49caa2ef9591ed88f01da74a1e415938270
Kim Do <kim.h.do.seven@gmail.com>
2019-03-05 12:37:48 -0800

    update play component

--- square_puzzle/public/src/app/materials.ts
+++ square_puzzle/public/src/app/materials.ts
@@ -1,12 +1,22 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material"
+import { MatButtonModule, MatSortModule, MatPaginatorModule } from "@angular/material"
+import { MatListModule } from '@angular/material/list'
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
     imports: [
-        MatButtonModule
+        MatButtonModule, 
+        MatListModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
     ],
     exports: [
-        MatButtonModule
+        MatButtonModule, 
+        MatListModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
     ]
 })
 
